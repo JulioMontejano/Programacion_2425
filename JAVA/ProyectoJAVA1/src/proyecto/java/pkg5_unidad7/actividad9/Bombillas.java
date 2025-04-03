d117061b29405d0a76ce52c99b6bd01c646be45c
Julio Montejano <montejanojulio@gmail.com>
2025-04-03 07:23:14 +0200

    A medias de hacer

---
+++ JAVA/ProyectoJAVA1/src/proyecto/java/pkg5_unidad7/actividad9/Bombillas.java
@@ -0,0 +1,40 @@
+package proyecto.java.pkg5_unidad7.actividad9;
+import java.lang.Math;
+import java.util.Scanner;
+
+public class Bombillas {
+    //atributos de clase
+    private boolean encendida;
+    private int numBombillas=(int) (Math.random()*500+1);
+
+    //setters y getters
+    boolean getEncendida() {
+        return encendida;
+    }
+
+    void setEncemdida(boolean encendida){
+        this.encendida=encendida;
+    }
+
+    int getNumBombillas(){
+        return numBombillas;
+    }
+
+    //metodos de la clase
+    boolean estado(boolean encendida){
+        @SuppressWarnings("resource")
+        Scanner leer=new Scanner(System.in);
+        boolean interruptor = false;
+        
+        System.out.println("¿Quieres apagar o encender la bombilla?; Si-> s, No-> n");
+        String eleccion=leer.next();
+        String letras=eleccion.toLowerCase();
+        if (letras.equals("s")) {
+            interruptor=true;
+        }else if (letras.equals("n")) {
+            interruptor=false;
+        } else{
+            System.out.println("Parámetro no comprendido");
+        }
+        return interruptor;
+    }}
---
+++ JAVA/ProyectoJAVA1/src/proyecto/java/pkg5_unidad7/actividad9/BombillasMain.java
@@ -0,0 +1,7 @@
+package proyecto.java.pkg5_unidad7.actividad9;
+
+public class BombillasMain {
+    public static void main(String[] args) {
+        
+    }
+}
